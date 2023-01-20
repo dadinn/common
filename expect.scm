07d16cae1c1d64f030280c88abb075cb50ffb092
Daniel Dinnyes <dadinn@besenczy.net>
2023-01-20 01:36:04 +0000

    added expect module
    This is a hygienic rewrite of ice-9 expect macros, which tries to fix
    couple of problems with the original non-higienic defmacro versions.
    
    ice-9 expect has support for procedures as test expressions in its
    clauses, but it keeps evaluating such expressions upon every char
    read from the port.
    
    Instead the test expression creating the matcher procedure should be
    evaluated only once, and that procedure should be stored in a binding,
    and referenced only as the test expression in the clauses themselves.
    
    Since (ice-9 expect) still uses un-hygienic defmacro, which cannot
    work together with the hygienic macro system, it was necessary
    to completely rewrite the expect implementation hygienically.

---
+++ expect.scm
@@ -0,0 +1,108 @@
+(define-module (common expect)
+  #:export
+  (expect-timeout expect-timeout-proc
+   expect-char-proc expect-eof-proc
+   expect-strings-compile-flags
+   expect-strings-exec-flags
+   expect-port))
+
+(add-to-load-path
+ (dirname (dirname (current-filename))))
+
+(define expect-port (current-input-port))
+(define expect-timeout #f)
+(define expect-timeout-proc #f)
+(define expect-eof-proc #f)
+(define expect-char-proc #f)
+(define expect-strings-compile-flags regexp/newline)
+(define expect-strings-exec-flags regexp/noteol)
+
+(define (expect-select port timeout)
+  (let* ((secs-usecs (gettimeofday))
+         (relative
+          (- timeout
+             (car secs-usecs)
+             (/ (cdr secs-usecs)
+                ;; one million.
+                1000000))))
+    (and (> relative 0)
+         (pair?
+          (car
+           (select
+            (list port)
+            '() '()
+            relative))))))
+
+(define-syntax expect-with-bindings
+  (lambda (stx)
+    (syntax-case stx (=>)
+
+      ((expect-with-bindings
+        (procedure-bindings ...)
+        (clauses-with-bindings ...)
+        ((matcher => consumer) more-clauses ...))
+       #'(expect-with-bindings
+          (procedure-bindings
+           ...
+           (matcher-binding matcher))
+          (clauses-with-bindings
+           ...
+           (matcher-binding-binding => (lambda (result) (apply consumer result))))
+          (more-clauses ...)))
+
+      ((expect-with-bindings
+        (procedure-bindings ...)
+        (clauses-with-bindings ...)
+        ((matcher body ...) more-clauses ...))
+       #'(expect-with-bindings
+          (procedure-bindings
+           ...
+           (matcher-binding matcher))
+          (clauses-with-bindings
+           ...
+           (matcher-binding body ...))
+          (more-clauses ...)))
+
+      ((expect-with-bindings
+        (procedure-bindings ...)
+        ((matcher-binding body ...) ...)
+        ())
+       (with-syntax ((expect-port (datum->syntax stx 'expect-port))
+                     (expect-timeout (datum->syntax stx 'expect-timeout))
+                     (expect-timeout-proc (datum->syntax stx 'expect-timeout-proc))
+                     (expect-char-proc (datum->syntax stx 'expect-char-proc))
+                     (expect-eof-proc (datum->syntax stx 'expect-eof-proc)))
+         #'(let ((input-port (or expect-port (current-input-port)))
+                 (timeout
+                  (if expect-timeout
+                      (let ((secs-usecs (gettimeofday)))
+                        (+ (car secs-usecs)
+                           expect-timeout
+                           (/ (cdr secs-usecs)
+                              ;; one million.
+                              1000000)))
+                      #f))
+                 procedure-bindings
+                 ...
+                 (content ""))
+             (let loop ()
+               (if (and expect-timeout (not (expect-select input-port timeout)))
+                   (and expect-timeout-proc (expect-timeout-proc content))
+                   (let* ((char (read-char input-port))
+                          (eof? (eof-object? char)))
+                     (when expect-char-proc
+                       (expect-char-proc char))
+                     (when (not eof?)
+                       (set! content
+                        (string-append content (string char))))
+                     (cond
+                      ((matcher-binding content eof?) body ...)
+                      ...
+                      (eof? (and expect-eof-proc (expect-eof-proc content)))
+                      (else (loop))))))))))))
+
+(define-syntax expect
+  (lambda (stx)
+    (syntax-case stx ()
+      ((expect clause clauses ...)
+       #'(expect-with-bindings () () (clause clauses ...))))))
