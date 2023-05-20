de44b074a88797e70d1824375ef3505beb05d8d5
Daniel Dinnyes <dadinn@besenczy.net>
2023-05-20 14:52:06 +0100

    changed signature of matcher procedures in clauses
    Backwards incompatible change to 2nd parameter of matcher procedures
    to receive either character, or a false value (in case of EOF object),
    instead of the original eof? boolean parameter.

--- expect.scm
+++ expect.scm
@@ -62,10 +62,9 @@
         ((matcher-binding body ...) ...)
         () ; no more clauses
         ;; other parameters
-        (expect-port expect-char-proc expect-eof-proc
+        (expect-port expect-eof-proc
          expect-timeout-proc expect-timeout))
        #'(let* ((input-port expect-port)
-                (char-proc expect-char-proc)
                 (eof-proc expect-eof-proc)
                 (timeout-proc expect-timeout-proc)
                 (timeout expect-timeout)
@@ -83,19 +82,20 @@
              (if (and timeout (not (expect-select input-port timeout)))
                  (and timeout-proc (timeout-proc content))
                  (let* ((char (read-char input-port))
-                        (eof? (eof-object? char))
+                        (char (and (not (eof-object? char)) char))
                         (next-content
-                         (or (and (not eof?)
+                         (or (and char
                                   (string-append
                                    content (string char)))
                              content)))
-                   (when char-proc
-                     (char-proc char))
                    (cond
-                    ((matcher-binding next-content eof?) body ...)
+                    ((matcher-binding next-content char)
+                     body ...)
                     ...
-                    (eof? (and eof-proc (eof-proc content)))
-                    (else (loop next-content)))))))))))
+                    ((not char)
+                     (and eof-proc (eof-proc content)))
+                    (else
+                     (loop next-content)))))))))))
 
 (define-syntax expect
   (lambda (stx)
@@ -105,9 +105,6 @@
            ((expect-port
              (or (syntax-capture #'expect 'expect-port)
                  (syntax (current-input-port))))
-            (expect-char-proc
-             (or (syntax-capture #'expect 'expect-char-proc)
-                 (syntax #f)))
             (expect-eof-proc
              (or (syntax-capture #'expect 'expect-eof-proc)
                  (syntax #f)))
@@ -119,6 +116,6 @@
                  (syntax #f))))
          #'(expect-with-bindings
             () () (clause clauses ...)
-            (expect-port expect-char-proc expect-eof-proc
+            (expect-port expect-eof-proc
              expect-timeout-proc expect-timeout)))))))
 
